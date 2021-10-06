099c61d2ba1fb54332cb13004f8cf800f8f41ef0
kyle <studio11keyuniverse@gmail.com>
2021-10-07 12:38:46 +1300

    BaseGame
    Input Space

---
+++ Assets/Script/Monster1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster1 : MonoBehaviour
+{
+    Player player;
+
+
+        public float Monster1Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Monster1Health = 50;
+        //player.monster1health = ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Monster1Health -= 1;
+            Debug.Log("Hit");
+
+        }
+
+        if (Monster1Health == 0)
+       {
+            Destroy(gameObject);
+       }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject[] ParticleSpawns = null;
+    public GameObject[] BloodParticleSpawn = null;
+    public GameObject bloodparticle = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (GameObject obj in ParticleSpawns)
+            {
+                Instantiate(bloodparticle, obj.transform.position, obj.transform.rotation);
+            }
+
+        }
+
+
+    }
+}
